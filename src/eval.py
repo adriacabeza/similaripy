e1fd4d0fb977c8f8fa959517f029aad797e48649
Albert Suarez <alsumo95@gmail.com>
2019-05-18 16:21:15 +0200

    Added eval

---
+++ src/eval.py
@@ -0,0 +1,54 @@
+import argparse
+import itertools
+import json
+
+from tqdm import tqdm
+
+from src.util.timer import Timer
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('validation_duplicates_file', type=str)
+    parser.add_argument('clusters_file', type=str)
+    return parser.parse_args()
+
+
+def _chunks(l, n):
+    return list(zip(*[iter(l)] * n))
+
+
+def _eval_dup(dup, clusters):
+    for cluster in clusters:
+        if set(dup).issubset(cluster):
+            return True
+    return False
+
+
+def get_matrix():
+    with Timer('Compute accuracy'):
+
+        with Timer('Load Validation duplicates JSON'):
+            with open(args.validation_duplicates_file) as f:
+                duplicates_requisites = json.load(f)
+                duplicates_requisites = duplicates_requisites['requirements']
+                duplicates_requisites = _chunks(duplicates_requisites, 2)
+
+        with Timer('Load clusters JSON'):
+            with open(args.clusters_file) as f:
+                clusters = json.load(f)
+
+        with Timer('Eval clusters'):
+            correct = 0
+            total_groups = len(duplicates_requisites)
+            for dup in tqdm(duplicates_requisites, total=total_groups):
+                correct += _eval_dup(dup, clusters)
+
+        print(correct)
+        print(total_groups)
+        print('{}%'.format((correct / total_groups) * 100))
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    get_matrix()

853237f9955cd60120e71c6d1912b4ba62f1f1e6
adria <adriacabeza@hotmail.com>
2019-05-18 15:42:21 +0200

    find clusters

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,8 @@ INDEX_FILE_NAME = 'index.nmslib'
 JSON_FILE_NAME = 'score_matrix.json'
 MAPPING_FILE_NAME = 'mapping.json'
 OUTPUT_PATH = 'data'
-DISTANCE_THRESHOLD = 0.5
+NEIGHBOURHOOD_AMOUNT = 1
+DISTANCE_THRESHOLD = 0.0000001
 
 BUILD_MODEL_ENDPOINT = 'http://localhost:9405/upc/Compare/BuildModel'
 COMPUTE_CLUSTERS_ENDPOINT = 'http://localhost:9405/upc/Compare/ComputeClusters'
@@ -12,6 +13,7 @@ __all__ = [
     'INDEX_FILE_NAME',
     'JSON_FILE_NAME',
     'MAPPING_FILE_NAME',
+    'NEIGHBOURHOOD_AMOUNT',
     'OUTPUT_PATH',
     'DISTANCE_THRESHOLD',
     'BUILD_MODEL_ENDPOINT',
---
+++ src/find_clusters.py
@@ -0,0 +1,75 @@
+import os
+import argparse
+import json
+
+
+from tqdm import tqdm
+
+from src import *
+from src.util import log
+from src.util import helper
+from src.util.nmslib import Nmslib
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_path', type=str)
+    return parser.parse_args()
+
+
+def _search(index):
+    log.info('Loading JSON...')
+    json_path_score = os.path.join(args.data_path, JSON_FILE_NAME)
+    json_path_mapping = os.path.join(args.data_path, MAPPING_FILE_NAME)
+    mapping = helper.load_json(json_path_mapping)
+    score_matrix = helper.load_json(json_path_score)
+    clusters = {}
+    next_id = 0
+    req_to_cl = {}
+
+    pbar = tqdm(total= len(score_matrix))
+    for i, embd in enumerate(score_matrix):
+        embd = helper.to_nparray(embd)
+        closest, distances = index.query(embd, NEIGHBOURHOOD_AMOUNT)
+
+        similar = {i}
+        for j, dist in zip(closest, distances):
+            if j != i:
+                if dist < DISTANCE_THRESHOLD:
+                    similar.add(int(j))
+
+        # merge clusters with previously found if possible
+        matching_clusters = set()
+        for j in similar:
+            if j in req_to_cl:
+                matching_clusters.add(req_to_cl[j])
+
+        for cl in matching_clusters:
+            similar.update(clusters.pop(cl))
+
+        # add new cluster to list
+        cl_id = next_id
+        next_id += 1
+
+        clusters[cl_id] = similar
+        for j in similar:
+            req_to_cl[j] = cl_id
+        
+        pbar.update(1)
+    
+    return list(clusters)
+
+
+
+def find_clusters(index):
+    clusters = _search(index)
+    with open(os.path.join(OUTPUT_PATH, 'clusters.json'), 'w') as f:
+        json.dump(clusters,f)
+
+    
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    index = Nmslib()
+    index.load(fn=os.path.join(args.data_path, INDEX_FILE_NAME))
+    find_clusters(index)

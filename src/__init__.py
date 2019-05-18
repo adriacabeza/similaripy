87f65959b781d283fda92faec2130e754cfe8f93
Albert Suarez <alsumo95@gmail.com>
2019-05-18 12:58:47 +0200

    Added more variables to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,14 @@
-
 INDEX_FILE_NAME = 'index.nmslib'
 JSON_FILE_NAME = 'matrix.json'
 OUTPUT_PATH = 'data'
 DISTANCE_THRESHOLD = 0.5
 
+COMPUTE_CLUSTERS_ENDPOINT = 'http://localhost:9405/upc/Compare/ComputeClusters'
+
 
 __all__ = [
     'INDEX_FILE_NAME',
-    'CSV_FILE_NAME',
+    'JSON_FILE_NAME',
     'OUTPUT_PATH',
+    'COMPUTE_CLUSTERS_ENDPOINT'
 ]

6e02e67b770e1fe8d27639562033abce3ac13a45
Albert Suarez <alsumo95@gmail.com>
2019-05-18 16:21:09 +0200

    Improved find_clusters

--- src/find_clusters.py
+++ src/find_clusters.py
@@ -1,8 +1,10 @@
 import os
 import argparse
 import json
+import numpy as np
 
 from tqdm import tqdm
+
 
 from src import *
 from src.util import log
@@ -64,12 +66,37 @@ def _search(index):
     return similars
 
 
+def _metrics(clusters):
+    total_requirements = int(np.concatenate(clusters).shape[0])
+    print('total_requirements: {}'.format(total_requirements))
+
+    dup_clusters = list(filter(lambda x: len(x) > 1, clusters))
+    dup_requirements = int(np.concatenate(dup_clusters).shape[0]) if dup_clusters else 0
+    print('similar_requirements: {}'.format(dup_requirements))
+
+    unique_requirements = total_requirements - dup_requirements
+    print('unique_requirements: {}'.format(unique_requirements))
+
+    avg_cluster_size = float(np.mean(list(map(len, dup_clusters)))) if dup_requirements else 0
+    print('avg_cluster_size: {}'.format(avg_cluster_size))
+
+    max_cluster_size = int(np.max(list(map(len, dup_clusters)))) if dup_requirements else 0
+    print('max_cluster_size: {}'.format(max_cluster_size))
+
+    similar_pct = (dup_requirements / total_requirements) * 100
+    print('pct_duplicates: {}%'.format(similar_pct))
+
+    amount_clusters = len(clusters)
+    print('amount_clusters: {}'.format(amount_clusters))
+
+
 def find_clusters():
     index = Nmslib()
     index.load(fn=os.path.join(args.data_path, INDEX_FILE_NAME))
     clusters = _search(index)
     with open(os.path.join(OUTPUT_PATH, 'clusters.json'), 'w') as f:
         json.dump(clusters, f)
+    _metrics(clusters)
 
 
 if __name__ == '__main__':

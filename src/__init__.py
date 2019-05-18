9525d81d17ae287dcf193ca910b4297280b2e496
Albert Suarez <alsumo95@gmail.com>
2019-05-18 14:22:23 +0200

    Added more global variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 INDEX_FILE_NAME = 'index.nmslib'
-JSON_FILE_NAME = 'matrix.json'
+JSON_FILE_NAME = 'score_matrix.json'
+MAPPING_FILE_NAME = 'mapping.json'
 OUTPUT_PATH = 'data'
 DISTANCE_THRESHOLD = 0.5
 
@@ -10,7 +11,9 @@ COMPUTE_CLUSTERS_ENDPOINT = 'http://localhost:9405/upc/Compare/ComputeClusters'
 __all__ = [
     'INDEX_FILE_NAME',
     'JSON_FILE_NAME',
+    'MAPPING_FILE_NAME',
     'OUTPUT_PATH',
+    'DISTANCE_THRESHOLD',
     'BUILD_MODEL_ENDPOINT',
     'COMPUTE_CLUSTERS_ENDPOINT'
 ]
